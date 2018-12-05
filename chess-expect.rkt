e2647b5a205b3d540d7bcd12a5e3ee76b290b354
Steeven9 <stefano.taille@gmail.com>
2018-12-05 09:39:02 +0100

    scaled images, added external config and diary

--- chess-expect.rkt
+++ chess-expect.rkt
@@ -20,9 +20,11 @@
 ; Libraries
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require racket/base)
 (require 2htdp/image)
 (require 2htdp/universe)
 
+(require "config.rkt")
 (require "pieces.rkt")
 
 
@@ -45,26 +47,20 @@
 ; Constants
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Screen width
-(define WIDTH 800)
-
-; Screen height
-(define HEIGHT 800)
-
 ; Initial empty world.
 ; Starting positions are the king for p1 and the topmost black tile for black,
 ; which is used to color the chessboard. After draw-map, it will be set to its king too.
 (define INITIAL-WORLD (make-world (empty-scene WIDTH HEIGHT)
                                   3 7
                                   1 0
-                                  #false
+                                  #true
                                   #false))
 
 ; A white tile
 (define W-TILE (square (/ WIDTH 8) 'solid 'white))
 
 ; A black tile
-(define B-TILE (square (/ WIDTH 8) 'solid 'black))
+(define B-TILE (square (/ WIDTH 8) 'solid 'brown))
 
 ; The white cursor
 (define W-CURSOR (square (/ WIDTH 8) 'outline (pen 'red 5 "dot" "round" "bevel")))
@@ -83,17 +79,16 @@
 (define (draw-board-line w)
   (cond [(< 7 (world-pos2x w)) (world-scene w)]
         [else
-         (place-image/align B-TILE
-                            (tile-x (world-pos2x w))
-                            (tile-y (world-pos2y w))
-                            "left" "top" 
-                            (draw-board-line (make-world (world-scene w)
-                                                         (world-pos1x w)
-                                                         (world-pos1y w)
-                                                         (+ 2 (world-pos2x w))
-                                                         (world-pos2y w)
-                                                         #false
-                                                         #false)))]))
+         (place-image B-TILE
+                      (tile-x (world-pos2x w))
+                      (tile-y (world-pos2y w))
+                      (draw-board-line (make-world (world-scene w)
+                                                   (world-pos1x w)
+                                                   (world-pos1y w)
+                                                   (+ 2 (world-pos2x w))
+                                                   (world-pos2y w)
+                                                   #false
+                                                   #false)))]))
 
 
 ; draw-board: World -> Image
@@ -114,51 +109,58 @@
                                  (world-pos1y w)
                                  0
                                  (+ 1 (world-pos2y w))
-                                 #false
+                                 #true
                                  #false))]
         [else (draw-board (make-world (draw-board-line w)
                                       (world-pos1x w)
                                       (world-pos1y w)
                                       1
                                       (+ 1 (world-pos2y w))
-                                      #false
+                                      #true
                                       #false))]))
 
 
 ; draw-cursors: World -> Image
 ; Draws the players cursors.
 (define (draw-cursors w)
-  (place-image/align W-CURSOR
-                     (tile-x (world-pos1x w))
-                     (tile-y (world-pos1y w))
-                     "left" "top"
-                     (place-image/align B-CURSOR
-                                        (tile-x (world-pos2x w))
-                                        (tile-y (world-pos2y w))
-                                        "left" "top"
-                                        (world-scene w))))
+  (place-image W-CURSOR
+               (tile-x (world-pos1x w))
+               (tile-y (world-pos1y w))
+               (place-image B-CURSOR
+                            (tile-x (world-pos2x w))
+                            (tile-y (world-pos2y w))
+                            (world-scene w))))
 
 
 ; tile-x: NonNegInteger -> NonNegNumber
 ; Return the x coordinate corresponding to the given column number.
 (define (tile-x col)
-  (* col (/ WIDTH 8)))
+  (+ (/ WIDTH 16) (* col (/ WIDTH 8))))
 
 ; Tests
-(check-expect (tile-x 0) 0)
-(check-expect (tile-x 1) 100)
-(check-expect (tile-x 6) 600)
+(check-expect (tile-x 0) 50)
+(check-expect (tile-x 1) 150)
+(check-expect (tile-x 6) 650)
 
   
 ; tile-y: NonNegInteger -> NonNegNumber
 ; Return the y coordinate corresponding to the given row number.
 (define (tile-y row)
-  (* row (/ HEIGHT 8)))
+  (+ (/ HEIGHT 16) (* row (/ HEIGHT 8))))
 
 ; Tests
-(check-expect (tile-y 0) 0)
-(check-expect (tile-y 1) 100)
-(check-expect (tile-y 6) 600)
+(check-expect (tile-y 0) 50)
+(check-expect (tile-y 1) 150)
+(check-expect (tile-y 6) 650)
+
+
+; handle-key: World Key -> World
+; Handles keypresses.
+(define (handle-key w key)
+  (cond [(key=? key "escape") (draw-board INITIAL-WORLD)]
+        [(and (key=? key "w") (world-mov1 w)) (draw-board INITIAL-WORLD)]
+        [(and (key=? key "up") (world-mov2 w)) (draw-board INITIAL-WORLD)]
+        [else w]))
   
 
 ; main: Nothing -> Nothing
@@ -166,6 +168,5 @@
 (define (main _)
   (big-bang INITIAL-WORLD
     [name "Chess-expect!"]
-    [to-draw draw-board]))
-;    [on-mouse handle-mouse]
-;    [on-key handle-key]))
+    [to-draw draw-board]
+    [on-key handle-key]))
---
+++ config.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname config) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+; PF1 final project: chess-expect!
+; Config file
+; Author: Stefano Taillefert
+
+(require racket/base)
+
+(provide WIDTH HEIGHT)
+
+; Screen width
+(define WIDTH 800)
+
+; Screen height
+(define HEIGHT 800)
